b842b4bef729229fb1884b1d8cb89bc4b630fb4f
Bokhari-Lotfi <lotfibk97@protonmail.com>
2018-03-29 10:09:58 +0100

    I did some shitty migrations

---
+++ database/migrations/2018_03_28_214736_create_collaboration_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollaborationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaborations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('publication_id');
+            $table->foreign('publication_id')->references('id')->on('publications');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collaborations');
+    }
+}
---
+++ database/migrations/2018_03_28_214803_create_publication_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('publications', function (Blueprint $table) {
+            $table->string('description');
+            $table->increments('id');
+            $table->string('title','30');
+           $table->unsignedInteger('owner');
+            $table->foreign('owner')->references('id')->on('users');
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publications');
+    }
+}
