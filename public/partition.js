107ec8a875d7c0ebc22fbc6d392b3907f71deb60
mounir4023 <dev.mounir@protonmail.com>
2018-04-01 16:16:11 +0100

    content selection and properties update done

--- public/partition.js
+++ public/partition.js
@@ -17,29 +17,43 @@ $(document).ready(function() {
       current_scroll=this.scrollTop;
     });
 
+    // scroll to last visit scroll
+    fixScroll(current_scroll);
+
+    // select the last visit selected content
+    selectContent(selected_content);
+
 });
 
 
 ///////////////////////// declaring variables  /////////////////////////
 
-// use blade to populate content-spaces depending on "displayed attribute"
 var content_spaces=document.querySelectorAll(".content-space");
 var grid_board=document.querySelector(".grid-board");
 
-var selected_content;
-var selected_content_space=$('.content-space')[0];
-
+var selected_content_space=document.getElementById("cs-"+selected_content);
 var selected_top=1,selected_left=3,selected_height=6,selected_width=7;
-var owner,creator,responsible,type;
 
-///////////////////////// fixing issues vars
-var current_scroll=0;
-var grid_rows=20;
+var inf_title=document.querySelector(".target-inf h5");
+var inf_description=document.querySelector(".target-inf span");
+var inf_owner=document.querySelector(".properties-basket p:nth-child(5)");
+var inf_creator=document.querySelector(".properties-basket p:nth-child(6)");
+var inf_responsible=document.querySelector(".properties-basket p:nth-child(7)");
+var inf_type=document.querySelector(".properties-basket p:nth-child(8)");
+var inf_top=document.querySelector(".inf-top");
+var inf_left=document.querySelector(".inf-left");
+var inf_width=document.querySelector(".inf-width");
+var inf_height=document.querySelector(".inf-height");
+var inf_cv=document.querySelector("#vertical-center");
+var inf_ch=document.querySelector("#horizontal-center");
+
 
 
 ///////////////////////// attaching listeners   /////////////////////////
 
 $('.add-to-board').click(createContentSpace);
+$('.content-space').click(chooseContent);
+$('.contents-basket .card-panel').click(chooseContent);
 
 $('.move-top').click(moveTop);
 $('.move-bot').click(moveBottom);
@@ -53,6 +67,71 @@ $('.less-height').click(lessHeight);
 
 ///////////////////////// defining functions   /////////////////////////
 
+// add a new content from the basket to the display board
+function createContentSpace() {
+
+  var id=getContentId(this.parentElement.id);
+
+  var description=document.createElement("span");
+  description.innerHTML=contents[id]["description"];
+  var title=document.createElement("h5");
+  title.innerHTML=contents[id]["title"];
+
+  var content=document.createElement("div");
+  content.classList.add("content-itself");
+  content.classList.add("card-panel");
+  content.classList.add("deep-orange-text");
+  content.appendChild(title);
+  content.appendChild(description);
+
+  var space=document.createElement("div");
+  space.classList.add("content-space");
+  space.id="cs-"+id;
+  space.appendChild(content);
+  space.style.gridArea=""+contents[id]["top"]+"/"+contents[id]["left"]+"/ span "+contents[id]["height"]+"/ span "+contents[id]["width"];
+  space.addEventListener("click",chooseContent);
+
+  grid_board.appendChild(space);
+  contents[id]["displayed"]=true;
+}
+
+// choose content by click
+function chooseContent() {
+  if ( contents[getContentId(this.id)]["displayed"])
+  selectContent(getContentId(this.id));
+}
+
+// Set the controls to edit the new selected content
+function selectContent(id) {
+
+  selected_content=id; console.log("id:"+selected_content);
+  selected_content_space=document.getElementById("cs-"+selected_content);
+
+  selected_top=contents[id]["top"];
+  selected_left=contents[id]["left"];
+  selected_width=contents[id]["width"]; 
+  selected_height=contents[id]["height"];
+
+  updateProperties();
+}
+
+// update the displayed properties by the new ones
+function updateProperties() {
+
+  inf_title.innerHTML=contents[selected_content]["title"];
+  inf_description.innerHTML=contents[selected_content]["description"];
+
+  inf_owner.innerHTML=contents[selected_content]["owner"];
+  inf_creator.innerHTML=contents[selected_content]["creator"];
+  inf_responsible.innerHTML=contents[selected_content]["responsible"];
+  inf_type.innerHTML=contents[selected_content]["type"];
+
+  inf_top.value=selected_top;
+  inf_left.value=selected_left;
+  inf_width.value=selected_width;
+  inf_height.value=selected_height;
+}
+
 function moveContent() {
   selected_content_space.style.gridArea=""+selected_top+" / "+selected_left+" / span "+selected_height+" / span "+selected_width;
   saveContentPosition(/* must add current params in case slow network*/);
@@ -63,7 +142,13 @@ function moveContent() {
 }
 
 function saveContentPosition() {
-  // Ajax post request to save the new position in the database
+
+  contents[selected_content]["top"]=selected_top;
+  contents[selected_content]["left"]=selected_left;
+
+  // Ajax post request to save the new position in the database post(contents[selected_content])
+
+  updateProperties();
 }
 
 function moveRight (){
@@ -102,7 +187,13 @@ function resizeContent() {
 }
 
 function saveContentSize() {
-  // Ajax post request to save the new size in the database
+
+  contents[selected_content]["width"]=selected_width;
+  contents[selected_content]["height"]=selected_height;
+
+  // Ajax post request to save the new size in the database post(contents[selected_content])
+
+  updateProperties();
 }
 
 function moreWidth() {
@@ -128,32 +219,6 @@ function lessHeight() {
   if (selected_height>1)
   selected_height--;
   resizeContent();
-}
-
-function createContentSpace() {
-  var id=getContentId(this.parentElement.id);
-
-  var description=document.createElement("span");
-  description.innerHTML=contents[id]["description"];
-  var title=document.createElement("h5");
-  title.innerHTML=contents[id]["title"];
-
-  var content=document.createElement("div");
-  content.classList.add("content-itself");
-  content.classList.add("card-panel");
-  content.classList.add("deep-orange-text");
-  content.appendChild(title);
-  content.appendChild(description);
-
-  var space=document.createElement("div");
-  space.classList.add("content-space");
-  space.id="cs-"+id;
-  space.appendChild(content);
-  space.style.gridArea=""+contents[id]["top"]+"/"+contents[id]["left"]+"/ span "+contents[id]["height"]+"/ span "+contents[id]["width"];
-  console.log(space.style.gridArea);
-
-  grid_board.appendChild(space);
-  contents[id]["displayed"]=true;
 }
 
 ////////////////////// fixing issues functions
