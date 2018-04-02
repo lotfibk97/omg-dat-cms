750f33dafc9c72e73fd4dddda271a33a23fa7c18
mounir4023 <dev.mounir@protonmail.com>
2018-04-02 15:15:58 +0100

    registration msg updated

--- resources/views/emails/message/created.blade.php
+++ resources/views/emails/message/created.blade.php
@@ -2,14 +2,17 @@
 
      Hello {{ $name }},
     <p>
-     Your signup process has been initiated. Please click the link below to
-      complete your registration and start getting paid.
+     Welcome in Omg-Dat-Cms, Your signup process has been initiated. In order to
+     complete the regristration please click in the link below :
     </p>
 
   <a href="{!!route('confirmation',['token' => $token])!!}">
       Confirm Account Now
-</a>
+  </a>
 
-     Thanks,<br>
-          {{ config('app.name') }} Team
+      Hope you enjoy easiness in managing content, and don't hesitate to scream
+      loudly "OH MY GOD THAT CMS IS AMASING"
+     <br>
+     <br>
+      {{ config('app.name') }} Team
       @endcomponent
