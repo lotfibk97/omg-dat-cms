32d4218f00cdf977c55d22eb3588da1c8581d47d
Bokhari-Lotfi <lotfibk97@protonmail.com>
2018-04-09 18:32:50 +0100

    still with profile

--- app/Http/Controllers/ProfileUpdaterController.php
+++ app/Http/Controllers/ProfileUpdaterController.php
@@ -7,32 +7,37 @@ use App\Models\Collaborator;
 use App\User;
 use App\Http\Requests\ProfileUpdateRequest;
 use Auth;
-
+use Storage;
 
 class ProfileUpdaterController extends Controller
 {
     public function update(ProfileUpdateRequest $request){
 // dd('a');
 $user = User::where('id',Auth::id())->first();
-// dd($user);
+// dd($user->type);
         if($user->type === 'profile'){
-// dd('a');
-       $collaborator= User::where('email',$user->email)->first();
-
+// // dd('a');
+         $collaborator= User::where('email',$user->email)->first();
+//
          if(!is_null($collaborator)){
+//
+//        $image = $request->file($request->all()['image']);
+// dd( $request->all()['image']);
+//        $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
+//
+//        $destinationPath = public_path('/static/images');
+//
+//        $image->move($destinationPath, $input['imagename']);
 
-       $image = $request->all()['image'];
-// dd($request->all()['image']);
-       $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
+         $url = $request->all()['image'];
+         $contents = file_get_contents($url);
+         $name = substr($url, strrpos($url, '/') + 1);
+         Storage::put($name, $contents);
 
-       $destinationPath = public_path('/static/images');
-
-       $image->move($destinationPath, $input['imagename']);
-
-       $collaborator->email = $request->email;
-       $collaborator->name = $request->name;
-       $collaborator->password = $request->password;
-       $collaborator->save();
+         $collaborator->email = $request->email;
+         $collaborator->name = $request->name;
+         $collaborator->password = $request->password;
+         $collaborator->save();
 }
 
     }
